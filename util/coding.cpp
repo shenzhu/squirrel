a2a37c65669f4dfefe7d80d705ef2c6057a318f1
Shen Zhu <shenzhu0127@gmail.com>
2019-06-08 13:17:10 -0400

    update coding

---
+++ util/coding.cpp
@@ -0,0 +1,39 @@
+#include "coding.h"
+
+namespace leveldb {
+
+namespace {
+
+bool kLittleEndian = true;
+
+}
+
+void EncodeFixed32(char* buf, uint32_t value) {
+	if (kLittleEndian) {
+		memcpy(buf, &value, sizeof(value));
+	}
+	else {
+		buf[0] = value & 0xff;
+		buf[1] = (value >> 8) & 0xff;
+		buf[2] = (value >> 16) & 0xff;
+		buf[3] = (value >> 24) & 0xff;
+	}
+}
+
+void EncodeFixed64(char* buf, uint64_t value) {
+	if (kLittleEndian) {
+		memcpy(buf, &value, sizeof(value));
+	}
+	else {
+		buf[0] = value & 0xff;
+		buf[1] = (value >> 8) & 0xff;
+		buf[2] = (value >> 16) & 0xff;
+		buf[3] = (value >> 24) & 0xff;
+		buf[4] = (value >> 32) & 0xff;
+		buf[5] = (value >> 40) & 0xff;
+		buf[6] = (value >> 48) & 0xff;
+		buf[7] = (value >> 56) & 0xff;
+	}
+}
+
+}  // namespace leveldb
--- util/coding.h
+++ util/coding.h
@@ -10,23 +10,31 @@
 
 namespace leveldb {
 
+namespace {
+
+bool kLittleEndian = true;
+
+}
+
+// Lower-level versions of Put... that write directly into a character buffer
+// REQUIRES: dst has enough space for the value being written
+extern void EncodeFixed32(char* dst, uint32_t value);
+extern void EncodeFixed64(char* dst, uint64_t value);
+
 // Lower-level versions of Get... that read directly from a character buffer
 // without any bounds checking.
 
 inline uint32_t DecodeFixed32(const char* ptr) {
-  // Windows is little endian, remove the part for big endian
-  // Load the raw bytes
-  uint32_t result;
-  memcpy(&result, ptr, sizeof(result));  // gcc optimizes this to a plain load
-  return result;
-  /*
-  } else {
-    return ((static_cast<uint32_t>(static_cast<unsigned char>(ptr[0]))) |
-            (static_cast<uint32_t>(static_cast<unsigned char>(ptr[1])) << 8) |
-            (static_cast<uint32_t>(static_cast<unsigned char>(ptr[2])) << 16) |
-            (static_cast<uint32_t>(static_cast<unsigned char>(ptr[3])) << 24));
-  }
-  */
+	if (kLittleEndian) {
+		uint32_t result;
+		memcpy(&result, ptr, sizeof(result));  // gcc optimizes this to a plain load
+		return result;
+	} else {
+		return ((static_cast<uint32_t>(static_cast<unsigned char>(ptr[0]))) |
+			    (static_cast<uint32_t>(static_cast<unsigned char>(ptr[1])) << 8) |
+				(static_cast<uint32_t>(static_cast<unsigned char>(ptr[2])) << 16) |
+				(static_cast<uint32_t>(static_cast<unsigned char>(ptr[3])) << 24));
+	}
 }
 
 }  // namespace leveldb
