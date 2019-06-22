7a711cc5bfee246972c555e2ed09d6d1b0ed6919
Shen Zhu <shenzhu0127@gmail.com>
2019-06-22 12:00:44 -0400

    add env implementation for posix

---
+++ util/env_posix_test_helper.h
@@ -0,0 +1,24 @@
+#ifndef STORAGE_LEVELDB_UTIL_ENV_POSIX_TEST_HELPER_H_
+#define STORAGE_LEVELDB_UTIL_ENV_POSIX_TEST_HELPER_H_
+
+namespace leveldb {
+
+	class EnvPosixTest;
+
+	// A helper for the POSIX Env to facilitate testing.
+	class EnvPosixTestHelper {
+	private:
+		friend class EnvPosixTest;
+
+		// Set the maximum number of read-only files that will be opened.
+		// Must be called before creating an Env.
+		static void SetReadOnlyFDLimit(int limit);
+
+		// Set the maximum number of read-only files that will be mapped via mmap.
+		// Must be called before creating an Env.
+		static void SetReadOnlyMMapLimit(int limit);
+	};
+
+}  // namespace leveldb
+
+#endif  // STORAGE_LEVELDB_UTIL_ENV_POSIX_TEST_HELPER_H_
